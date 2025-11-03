15942d0eec0bb9239243708ab6bb58476733f563
PhyoMinKhant112 <phyomin.24@ichat.sp.edu.sg>
2025-11-03 13:49:43 +0800

    Break test on purpose

--- _tests_/sum.test.js
+++ _tests_/sum.test.js
@@ -1,3 +1,6 @@
 // __tests__/sum.test.js
 import { sum } from '../src/sum.js';
-test('adds 2 + 2 = 4', () => { expect(sum(2, 2)).toBe(4); });
+
+test('adds 2 + 2 = 5', () => {  // intentionally wrong
+  expect(sum(2, 2)).toBe(5);
+});
